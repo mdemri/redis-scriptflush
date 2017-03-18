24280851eca46a6094c49957c6a1796714131ea2
Moshe Demri <mdemri@gmail.com>
2017-03-18 21:25:39 +0200

    process exit

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const Redis = require('ioredis');
 const _ = require('lodash');
 
-const debugStrs = ''
+const debugStrs = 'redis-17849.c3.eu-west-1-1.ec2.cloud.redislabs.com:17849'
+// ,redis-11997.c3.eu-west-1-1.ec2.cloud.redislabs.com:11997,redis-16160.c3.eu-west-1-1.ec2.cloud.redislabs.com:16160,redis-14770.c3.eu-west-1-1.ec2.cloud.redislabs.com:14770,redis-17419.c3.eu-west-1-1.ec2.cloud.redislabs.com:17419,redis-12455.c3.eu-west-1-1.ec2.cloud.redislabs.com:12455'
 const connStrs = process.env.CONN_STRS || debugStrs;
 
-const ConnectionStrings = connStrs.split(',');
 
-Promise.all(_.map(ConnectionStrings, x => new Redis(x).script('FLUSH'))).then(x => console.log(x));
+new Redis(debugStrs).script('FLUSH').then((x) => {
+    console.log(x);
+    process.exit(0);
+});
+
+// const ConnectionStrings = connStrs.split(',');
+// const arr = _.map(ConnectionStrings, x => new Redis(x));
+// Promise.all(_.map(arr, x => x.script('FLUSH'))).then(x => console.log(x));
